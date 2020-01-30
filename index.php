207876d82315b128d67417878ae19b40f3657712
missingindent <60475226+missingindent@users.noreply.github.com>
2020-01-30 17:49:48 -0500

    V5

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
 
         <div class="jumbotron jumbotron-fluid">
           <div class="container">
-            <h1 class="display-4">Fluid jumbotron</h1>
-            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            <h1>Missing Indent</h1>
+            <p class="lead">Wonderful world of web apps.</p>
           </div>
         </div>
         
