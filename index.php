457e453dc586771b56aca0758d5eb67d78b4a2b0
missingindent <60475226+missingindent@users.noreply.github.com>
2020-01-30 17:47:51 -0500

    V4

--- index.php
+++ index.php
@@ -11,8 +11,20 @@
     <title>Hello, world!</title>
   </head>
   <body>
-  <div class="container-fluid">
-    <h1>Hello, world!</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-lg">
+
+        <div class="jumbotron jumbotron-fluid">
+          <div class="container">
+            <h1 class="display-4">Fluid jumbotron</h1>
+            <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+          </div>
+        </div>
+        
+        
+      </div>
+    </div>
   </div>
     
     <!-- Optional JavaScript -->
