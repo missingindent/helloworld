3536acbb149010f43931679d9b4c9caaf7f817d2
missingindent <60475226+missingindent@users.noreply.github.com>
2020-01-30 17:45:47 -0500

    V3

--- index.php
+++ index.php
@@ -11,8 +11,10 @@
     <title>Hello, world!</title>
   </head>
   <body>
+  <div class="container-fluid">
     <h1>Hello, world!</h1>
-
+  </div>
+    
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
